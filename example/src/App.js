eb26e0bea139ac76887123ba0031a11465e37ee7
Thomas Shannon <thomasshannon1117@gmail.com>
2020-07-30 11:02:42 -0700

    added initial

---
+++ example/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+function Foo() {
+  return (
+    <div>
+      <p>Foo component</p>
+    </div>
+  )
+}
+
+function Bar() {
+  return (
+    <div>
+      <p>Bar component</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={Foo} />
+        <Route path="/bar" component={Bar} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
